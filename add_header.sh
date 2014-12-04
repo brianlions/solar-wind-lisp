b17fb32f71d4842d0fb94ecb776cc8681d04a75f
Brian Y. ZHANG <brianlions@gmail.com>
2014-12-04 23:30:59 +0800

    update add_header.sh

--- add_header.sh
+++ add_header.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 author_name="Brian Yi ZHANG"
 author_email="brianlions@gmail.com"
+date_format="%a %b %d %H:%M:%S %Y %Z"
+#date_format="%Y/%m/%d %H:%M:%S"
 
 for fn in "$@"
 do
     # get first commit id of the specified file
     commit_id=`git log --oneline "$fn" | tail -1 | gawk '{ print $1 }'`
     # get date and time from commit message of that commit
-    long_date=`git log $commit_id -1 | grep Date | cut -c 9-32`
+    log_date=`git log $commit_id -1 | grep Date | cut -c 9-32`
     # format the date and time
-    short_date=`date -d "$long_date" +"%Y/%m/%d %H:%M:%S"`
+    header_date=`date -d "$log_date" +"$date_format"`
 
     # add header to the file
     sed -i -e '1 i\
@@ -18,6 +20,6 @@ do
  *\
  * author:              '"$author_name"'\
  * email:               '"$author_email"'\
- * date created:        '"$short_date"'\
+ * date created:        '"$header_date"'\
  */' "$fn"
 done
